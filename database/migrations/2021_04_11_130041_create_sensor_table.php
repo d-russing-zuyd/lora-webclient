95f7faf4636bf3820e59ee524d55836076dca8b7
Damian <damianrussing@ziggo.nl>
2021-04-11 15:22:02 +0200

    Migrations (broken)

---
+++ database/migrations/2021_04_11_130041_create_sensor_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSensorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensor', function (Blueprint $table) {
+            $table->id();
+            $table->foreign('device_id')->references('id')->on('device')->onDelete('cascade');
+            $table->integer('type');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensor');
+    }
+}
